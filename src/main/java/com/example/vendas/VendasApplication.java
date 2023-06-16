e44501c2a5b3cae6efc33821129a94ea92592477
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-06-16 19:33:03 -0300

    projeto com springboot

---
+++ src/main/java/com/example/vendas/VendasApplication.java
@@ -0,0 +1,24 @@
+package com.example.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping ("/hello")
+    public String helloWord(){
+        return "hello word";
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+
+}
